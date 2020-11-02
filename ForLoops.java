57242d759f821a745e4d1ee58ceb41f8f9536dd5
Harshitha P <73736464+Harshithap07@users.noreply.github.com>
2020-11-02 16:03:39 +0530

    Add files via upload

---
+++ ForLoops.java
@@ -0,0 +1,14 @@
+class ForLoops{
+public static void main(String args[]){
+for(int i=0;i<8;i++){
+System.out.println("I Love DairyMilk");
+}
+for(int i=0;i<15;i++){
+System.out.println(i);
+}
+int[] grades={98,100,83,90,93};
+for(int i=0;i<grades.length;i++){
+System.out.println(grades[i]);
+}
+}
+}
